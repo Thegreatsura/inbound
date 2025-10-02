c8cf84f5c8e1f2e5e6679d4e9d87f499875554af
preet <preetsutharxd@gmail.com>
2025-10-02 21:56:13 +0530

    Update components/animated-height-div.tsx
    Co-authored-by: vercel[bot] <35613825+vercel[bot]@users.noreply.github.com>

--- components/animated-height-div.tsx
+++ components/animated-height-div.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import React, { useEffect, useRef, useState } from "react";
 
 interface AnimatedHeightDiv {
